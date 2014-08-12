6dca8e0d194ca87fdc6f90f54c680f4b5f3eda78
tanis <tanis-y@hotmail.com>
2014-08-12 03:11:00 -0400

    sql

--- wami.sql
+++ wami.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 10, 2014 at 10:35 PM
+-- Generation Time: Aug 12, 2014 at 09:10 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `identity_profiler` (
   `create_date` datetime NOT NULL,
   `modified_date` datetime NOT NULL,
   PRIMARY KEY (`identity_profiler_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=49 ;
 
 --
 -- Dumping data for table `identity_profiler`
@@ -50,46 +50,23 @@ CREATE TABLE IF NOT EXISTS `identity_profiler` (
 INSERT INTO `identity_profiler` (`identity_profiler_id`, `user_id`, `profile_id`, `category`, `media_type`, `file_type`, `profiler_url`, `title`, `file_name`, `description`, `delete_ind`, `create_date`, `modified_date`) VALUES
 (1, 0, 0, 'Pictures', 'Picture', 'png', '/wami/profilerdata/rlanter/pic/pic1.png', '', 'My Parents', 'This is a picture of my parents', 0, '2014-07-10 10:53:13', '2014-07-10 10:53:13'),
 (2, 0, 0, 'Pictures', 'Picture', 'png', '/wami/profilerdata/rlanter/pic/pic2.png', '', 'My Cat', '', 0, '2014-07-10 10:53:53', '2014-07-10 10:53:53'),
-(3, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/dog1.jpg', 'dog1', '', 'This is a picture of my dog', 0, '2014-07-10 15:05:34', '2014-07-10 15:05:34'),
-(4, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/dog2.jpg', '', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
-(5, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/doge.jpg', '', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
-(6, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/jb-dog.jpg', '', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
-(7, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/mdog.jpg', 'mdog', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
-(8, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/Cute-Dog-Wallpapers.jpg', '', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
-(9, 1, 1, 'Pictures', 'Picture', 'jpeg', 'profilerdata/jim/pic/dog.jpeg', '', 'Dog', 'A yellow dog.', 0, '2014-07-28 12:57:32', '2014-07-28 12:57:32'),
-(10, 1, 1, 'Pictures', 'Picture', 'jpg', 'profilerdata/jim/pic/plant1.jpg', '', 'Plant 1', 'Plant 1 image.', 0, '2014-07-28 13:01:03', '2014-07-28 13:01:03'),
-(11, 1, 1, 'Pictures', 'Picture', 'jpg', 'profilerdata/jim/pic/plant2.jpg', '', 'Plant 2', 'Plant 2 image.', 0, '2014-07-28 13:02:11', '2014-07-28 13:02:11'),
-(12, 1, 1, 'Pictures', 'Picture', 'jpg', 'profilerdata/jim/pic/plant3.jpeg', '', 'Plant 3', 'Plant 3 image.', 0, '2014-07-28 13:03:30', '2014-07-28 13:03:30'),
-(13, 1, 1, 'Pictures', 'Picture', 'jpg', 'profilerdata/jim/pic/plant4.jpg', '', 'Plant 4', 'Plant 4 image', 0, '2014-07-28 13:03:38', '2014-07-28 13:03:38'),
-(14, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/FlowChart.png', '', 'FlowChart.png', '', 0, '2014-07-28 13:03:38', '2014-07-28 13:03:38'),
-(24, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/flowbreeze-order-flowchart.png', 'afsda', 'flowbreeze-order-flowchart.png', 'asdgadg', 0, '2014-08-08 23:26:59', '2014-08-08 23:26:59'),
-(25, 18, 1, 'Pictures', 'Picture', 'jpeg', '/profilerdata/Tanis/pic/Darth.jpeg', 'darth', 'Darth.jpeg', 'adfasdf', 0, '2014-08-08 23:27:59', '2014-08-08 23:27:59'),
-(28, 18, 1, 'Pictures', 'Picture', 'jpg', '/profilerdata/Tanis/pic/m-sd_f.jpg', 'Chart', 'm-sd_f.jpg', 'Just\nA\nChart', 0, '2014-08-09 00:15:36', '2014-08-09 00:15:36'),
-(29, 18, 1, 'Pictures', 'Picture', 'jpg', '/profilerdata/Tanis/pic/figure1.jpg', 'ta', 'figure1.jpg', 'asd', 0, '2014-08-09 02:15:04', '2014-08-09 02:15:04'),
-(30, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/pic/F5F9CCAC-355E-46AC-A823-C8BC3EDD628F.png', '', 'F5F9CCAC-355E-46AC-A823-C8BC3EDD628F.png', '', 0, '2014-08-09 02:18:01', '2014-08-09 02:18:01');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(80) NOT NULL,
-  `delete_ind` tinyint(1) NOT NULL,
-  `create_date` datetime NOT NULL,
-  `modified_date` datetime NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=74 ;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `username`, `delete_ind`, `create_date`, `modified_date`) VALUES
-(18, 'Tanis', 0, '2014-07-30 00:00:00', '2014-08-02 00:00:00'),
-(73, '', 0, '2014-07-10 10:49:28', '2014-07-10 10:49:28');
+(3, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/0/pic/dog1.jpg', 'dog1', '', 'This is a picture of my dog', 0, '2014-07-10 15:05:34', '2014-07-10 15:05:34'),
+(4, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/0/pic/dog2.jpg', '', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
+(5, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/0/pic/doge.jpg', '', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
+(6, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/0/pic/jb-dog.jpg', '', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
+(7, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/0/pic/mdog.jpg', 'mdog', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
+(8, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/0/pic/Cute-Dog-Wallpapers.jpg', '', '', '', 0, '2014-07-10 15:05:38', '2014-07-10 15:05:38'),
+(14, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/FlowChart.png', '', 'FlowChart.png', '', 0, '2014-07-28 13:03:38', '2014-07-28 13:03:38'),
+(24, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/flowbreeze-order-flowchart.png', 'afsda', 'flowbreeze-order-flowchart.png', 'asdgadg', 0, '2014-08-08 23:26:59', '2014-08-08 23:26:59'),
+(25, 18, 1, 'Pictures', 'Picture', 'jpeg', '/profilerdata/Tanis/1/pic/Darth.png', 'darth', 'Darth.png', 'adfasdf', 2, '2014-08-08 23:27:59', '2014-08-08 23:27:59'),
+(28, 18, 1, 'Pictures', 'Picture', 'jpg', '/profilerdata/Tanis/1/pic/m-sd_f.jpg', 'Chart', 'm-sd_f.jpg', 'Just\nA\nChart', 0, '2014-08-09 00:15:36', '2014-08-09 00:15:36'),
+(29, 18, 1, 'Pictures', 'Picture', 'jpg', '/profilerdata/Tanis/1/pic/figure1.jpg', 'ta', 'figure1.jpg', 'asd', 0, '2014-08-09 02:15:04', '2014-08-09 02:15:04'),
+(34, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', 'Darth', 'Darth.png', '', 2, '2014-08-12 05:02:16', '2014-08-12 05:02:16'),
+(44, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/0/pic/Darth.png', '', 'Darth.png', '', 1, '2014-08-12 07:42:03', '2014-08-12 07:42:03'),
+(45, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 2, '2014-08-12 07:42:50', '2014-08-12 07:42:50'),
+(46, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 2, '2014-08-12 07:44:03', '2014-08-12 07:44:03'),
+(47, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 2, '2014-08-12 07:44:50', '2014-08-12 07:44:50'),
+(48, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 0, '2014-08-12 07:46:20', '2014-08-12 07:46:20');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
