e3b5d22b485e4daaedcd18faaeef6e3fd97f9972
tanis <tanis-y@hotmail.com>
2014-08-13 15:31:54 -0400

    sql

--- wami.sql
+++ wami.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 12, 2014 at 09:10 AM
+-- Generation Time: Aug 13, 2014 at 09:30 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `identity_profiler` (
   `create_date` datetime NOT NULL,
   `modified_date` datetime NOT NULL,
   PRIMARY KEY (`identity_profiler_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=49 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=57 ;
 
 --
 -- Dumping data for table `identity_profiler`
@@ -60,13 +60,15 @@ INSERT INTO `identity_profiler` (`identity_profiler_id`, `user_id`, `profile_id`
 (24, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/flowbreeze-order-flowchart.png', 'afsda', 'flowbreeze-order-flowchart.png', 'asdgadg', 0, '2014-08-08 23:26:59', '2014-08-08 23:26:59'),
 (25, 18, 1, 'Pictures', 'Picture', 'jpeg', '/profilerdata/Tanis/1/pic/Darth.png', 'darth', 'Darth.png', 'adfasdf', 2, '2014-08-08 23:27:59', '2014-08-08 23:27:59'),
 (28, 18, 1, 'Pictures', 'Picture', 'jpg', '/profilerdata/Tanis/1/pic/m-sd_f.jpg', 'Chart', 'm-sd_f.jpg', 'Just\nA\nChart', 0, '2014-08-09 00:15:36', '2014-08-09 00:15:36'),
-(29, 18, 1, 'Pictures', 'Picture', 'jpg', '/profilerdata/Tanis/1/pic/figure1.jpg', 'ta', 'figure1.jpg', 'asd', 0, '2014-08-09 02:15:04', '2014-08-09 02:15:04'),
+(29, 18, 1, 'Pictures', 'Picture', 'jpg', '/profilerdata/Tanis/1/pic/figure1.jpg', 'ta', 'figure1.jpg', 'asd', 1, '2014-08-09 02:15:04', '2014-08-09 02:15:04'),
 (34, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', 'Darth', 'Darth.png', '', 2, '2014-08-12 05:02:16', '2014-08-12 05:02:16'),
 (44, 18, 0, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/0/pic/Darth.png', '', 'Darth.png', '', 1, '2014-08-12 07:42:03', '2014-08-12 07:42:03'),
 (45, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 2, '2014-08-12 07:42:50', '2014-08-12 07:42:50'),
 (46, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 2, '2014-08-12 07:44:03', '2014-08-12 07:44:03'),
 (47, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 2, '2014-08-12 07:44:50', '2014-08-12 07:44:50'),
-(48, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 0, '2014-08-12 07:46:20', '2014-08-12 07:46:20');
+(48, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 2, '2014-08-12 07:46:20', '2014-08-12 07:46:20'),
+(49, 18, 2, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/2/pic/Plain_White_Ts-Hey_There_Delilah-AGuitar.png', '', 'Plain_White_Ts-Hey_There_Delilah-AGuitar.png', '', 0, '2014-08-12 23:35:30', '2014-08-12 23:35:30'),
+(50, 18, 1, 'Pictures', 'Picture', 'png', '/profilerdata/Tanis/1/pic/Darth.png', '', 'Darth.png', '', 1, '2014-08-13 03:35:04', '2014-08-13 03:35:04');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
